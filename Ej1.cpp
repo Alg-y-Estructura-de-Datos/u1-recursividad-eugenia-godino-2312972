dfea9cd327fac360ae0d6bdddea227fbc5b1ce01
euge7777 <2312972@ucc.edu.ar>
2024-08-07 19:54:54 -0300

    completado

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int potencia (int base, int exponente){
+    if (exponente == 0){
+        return 1;
+    } else {
+        base = base * potencia (base, exponente-1);
+    }
+    return base;
+}
+
 int main() {
-
-    cout << "Examen Final Prog 3" << endl;
-
-    return 0;
+    int x, y;
+    cout << "Ingrese base: " ;
+    cin >> x;
+    cout << "Ingrese exponente: ";
+    cin >> y;
+    cout << "La potencia de " << x << " elevado a la " << y << " es " << potencia(x, y);
 }

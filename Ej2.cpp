c179e29100c34c3a0e955d89b1c5e5ffbd2d47d9
euge7777 <2312972@ucc.edu.ar>
2024-08-07 20:06:12 -0300

    completado

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int suma (int a){ 
+    if ( a == 1){
+        return 1;
+    } else {
+    return a + (suma (a - 1));
+    }
+}
+
 int main() {
-
-    cout << "Examen Final Prog 3" << endl;
-
+    int a; 
+    cout << "Ingrese un numero: ";
+    cin >> a; 
+    cout << "La suma de todos los numeros hasta el " << a << " es de: " << suma (a) <<endl;
     return 0;
 }
